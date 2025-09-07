2307980932f94081616d844b6155b45e1e2e497a
Zach <zachgriffin1990@gmail.com>
2025-09-07 10:00:08 -0400

    Trigger GitHub Actions to test runner

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -24,3 +24,4 @@ num_lines=$(grep -r "$searchstr" "$filesdir" | wc -l)
 # Print the result
 echo "The number of files are $num_files and the number of matching lines are $num_lines"
 # Test Actions runner
+# Test Actions runner after removing large files
